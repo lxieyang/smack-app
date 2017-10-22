6ce0235694ace2ab2c47d405bd7405ca14a7a1ff
lxieyang <lxieyang@umich.edu>
2017-10-22 00:31:41 -0400

    fetch messages

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Xieyang Liu on 10/22/17.
+//  Copyright © 2017 Michael Liu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!   
+    
+}
